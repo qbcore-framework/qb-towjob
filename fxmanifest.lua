d72108b46b876ee74e23730053f76cd4cea8d001
Idris <idrisdev@gmail.com>
2021-11-21 16:47:28 +1100

    fix: removed plural script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,14 +2,10 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-TowJob'
-version '1.0.0'
+version '1.1.0'
 
 shared_script 'config.lua'
-
-client_scripts {
-    'client/main.lua',
-}
-
+client_script 'client/main.lua'
 server_script 'server/main.lua'
 
 lua54 'yes'
