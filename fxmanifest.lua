7dcf9d449bf15da3850b8314e027577ffcfb02a4
buddizer <none>
2022-09-22 09:53:50 -0600

    auto switch locale system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,16 @@ description 'QB-TowJob'
 version '1.1.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'config.lua',
-	'locales/en.lua',
+    '@qb-core/shared/locale.lua',
+    'config.lua',
+    'locales/en.lua',
     'locales/*.lua'
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua'
 }
 
